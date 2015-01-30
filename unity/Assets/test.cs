4488972cde3a7918eecca7da802b2f11669dc291
inkcomic <inkcomic@gmail.com>
2015-01-31 00:41:12 +0800

    update

---
+++ unity/Assets/test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int count = 10;// this.spawnAmount;
+        Transform inst;
+        SpawnPool shapesPool = PoolManager.Pools["bulletpool"];
+        //while (count > 0)
+        //{
+            // Spawn in a line, just for fun
+            inst = shapesPool.Spawn(this.transform);
+            inst.localPosition = new Vector3((10 + 2) - count, 0, 0);
+            count--;
+
+            //  yield return new WaitForSeconds(1);
+        //}
+
+        //  this.StartCoroutine(Despawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+}
